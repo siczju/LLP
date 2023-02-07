fe24eaead4ef9eeaeac30519d4bca41912b9f4ae
jcesargm <juliocesar.gm6000@gmail.com>
2023-02-07 14:18:39 -0300

    adding new exercises

--- lista-1-LLP/recursão/dec2bin.c
+++ lista-1-LLP/recursão/dec2bin.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
  
-void binary(int vet[], int index, int aux){
-    if(aux == 0)
-     vet[index] = aux;
+void binary(int binaryVetor[], int index, int denary){
+    if(denary == 0)
+     binaryVetor[index] = denary;
     else{
-     binary(vet, index - 1, aux / 2);
-     vet[index] = aux % 2;
+     binaryVetor[index] = denary % 2;
+     binary(binaryVetor, index - 1, denary / 2);
     }
 }
  
 void main(){
-    int aux, index, binario[8] = {0};
+    int denary, index, binario[8] = {0};
 
-    printf("type a denary value: "); scanf("%d",&aux);
+    printf("type a denary value: "); scanf("%d",&denary);
     
-    binary(binario, 7, aux);
+    binary(binario, 7, denary);
 
-    for(aux = 0; aux < 8; aux++)
-        printf("%d", binario[aux]);
+    for(denary = 0; denary < 8; denary++)
+        printf("%d", binario[denary]);
 }
